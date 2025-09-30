760b9edfeca536f5b70c2710d2f3657ed5be0355
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2025-10-01 01:40:15 +0200

    Update resenje.vhdl

--- lp/demultiplekser/resenje.vhdl
+++ lp/demultiplekser/resenje.vhdl
@@ -7,12 +7,12 @@ end entity;
 architecture demux_1to8_arch of demux_1to8 is 
   begin 
     with (Sel) select 
-    F(0) <= A when “000”; 
-    F(1) <= A when “001”; 
-    F(2) <= A when “010”; 
-    F(3) <= A when “011”;  
-    F(4) <= A when “100”; 
-    F(5) <= A when “101”; 
-    F(6) <= A when “110”; 
+    F(0) <= A when “000”,
+    F(1) <= A when “001”,
+    F(2) <= A when “010”, 
+    F(3) <= A when “011”,  
+    F(4) <= A when “100”, 
+    F(5) <= A when “101”, 
+    F(6) <= A when “110”, 
     F(7) <= A when “111”;  
 end architecture; 
