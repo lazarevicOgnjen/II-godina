2033e00708ebe6a8da0dc8270c96d357f6aa1ff2
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2025-09-30 17:29:08 +0200

    Update resenje.vhdl

--- lp/minterm/zadatak1/resenje.vhdl
+++ lp/minterm/zadatak1/resenje.vhdl
@@ -21,3 +21,33 @@ architecture SystemE_arch of SystemE is
     F <= m1 or m3 or m4 or m6;
 
 end architecture;
+
+-- lab vezbe dodatak
+
+entity SystemE_TB is
+end entity;
+  
+architecture SystemE_TB_arch of SystemE_TB is
+  
+  component SystemE
+    port (A,B,C : in bit;
+          F: out bit);
+  end component;
+  
+  signal A_TB, B_TB, C_TB : bit;
+  signal F_TB : bit;
+
+  begin
+    DUT1: SystemE port map (A => A_TB, B => B_TB, C => C_TB, F => F_TB);
+      
+    STIMULUS : process
+    begin
+      A_TB <= '0'; B_TB <= '1'; C_TB <='1'; wait for 100 ps;
+      A_TB <= '1'; B_TB <= '1'; C_TB <='1'; wait for 100 ps;
+    end process;
+        
+end architecture;
+
+
+
+      
