321a47c0f6a364140f57f81849c3dd58d00b5661
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2025-09-30 17:01:31 +0200

    Update resenje.vhdl

--- lp/memorija/RW/asinhrona/resenje.vhdl
+++ lp/memorija/RW/asinhrona/resenje.vhdl
@@ -28,3 +28,57 @@ architecture rw_16x8_async_arch of rw_16x8_async is
     end process;
         
 end architecture;
+
+entity rw_16x8_async_TB is
+end entity; 
+
+architecture rw_16x8_async_TB_arch of rw_16x8_async_TB is
+  component rw_16x8_async
+    port (address : in std_logic_vector(3 downto 0);
+          data_in : in std_logic_vector(7 downto 0));
+          WE : in std_logic;
+          data_out : out std_logic_vector(7 downto 0));
+  end component;
+        
+  signal address_TB : std_logic_vector(3 downto 0);  
+  signal data_in_TB : std_logic_vector(7 downto 0);
+  WE_TB : std_logic;
+  signal data_out_TB : std_logic_vector(7 downto 0);
+
+  begin
+    DUT1: rw_16x8_sync port map (address => address_TB,
+                                 data_in => data_in_TB,
+                                 WE => WE_TB,
+                                 data_out => data_out_TB); 
+
+    STIMULUS : process
+    signal i : integer;
+
+    begin
+      WE_TB <= 0;
+      for i in 0 to 15 loop
+        report “adress= “ & address_TB’image &
+        “ data_out= “ & data_out_TB’image;
+      end loop;
+      wait for 40 ns;
+        
+      WE_TB <= 1;
+      for i in 0 to 15 loop
+        address_TB <= std_logic_vector(to_unsigned(i,4));
+        data_in_TB <= std_logic_vector(to_unsigned(i,8));
+      end loop;
+      wait for 120 ns;
+        
+      WE_TB <= 0;
+      for i in 0 to 15 loop
+        report “adress= “ & address_TB’image &
+        “ data_out= “ & data_out_TB’image;
+      end loop;
+      wait for 200 ns;
+        
+    end process;
+end architecture; 
+
+
+
+
