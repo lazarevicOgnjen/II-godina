6a30d6169280dea87577331cf593abf5e6443acf
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2025-10-01 01:35:46 +0200

    Update resenje.vhdl

--- lp/demultiplekser/resenje.vhdl
+++ lp/demultiplekser/resenje.vhdl
@@ -7,19 +7,19 @@ end entity;
 architecture demux_1to8_arch of demux_1to8 is 
   begin 
     with (Sel) select 
-    F(7) <= A when “000”; 
+    F(0) <= A when “000”; 
     with (Sel) select 
-    F(6) <= A when “001”; 
+    F(1) <= A when “001”; 
     with (Sel) select 
-    F(5) <= A when “010”; 
+    F(2) <= A when “010”; 
     with (Sel) select 
-    F(4) <= A when “011”; 
+    F(3) <= A when “011”; 
     with (Sel) select 
-    F(3) <= A when “100”; 
+    F(4) <= A when “100”; 
     with (Sel) select 
-    F(2) <= A when “101”; 
+    F(5) <= A when “101”; 
     with (Sel) select 
-    F(1) <= A when “110”; 
+    F(6) <= A when “110”; 
     with (Sel) select 
-    F(0) <= A when “111”;  
+    F(7) <= A when “111”;  
 end architecture; 
