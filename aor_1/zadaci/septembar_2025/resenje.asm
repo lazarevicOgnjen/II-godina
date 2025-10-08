c19e6d3ee0663484d8ee36642980e16950ee72f6
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2025-10-09 01:48:54 +0200

    Update resenje.asm

--- aor_1/zadaci/septembar_2025/resenje.asm
+++ aor_1/zadaci/septembar_2025/resenje.asm
@@ -1,36 +1,56 @@
-section .data
-    N    dd 10         ; 32-bitni ulazni broj
-    SUM  dq 0          ; 64-bitna lokacija za rezultat
+; sum_expr.asm
+; računa SUM = sum_{i=5..n} ( (n+i)/(i-1) + (n-i)^2 )
+; N - dd (32-bit) ulaz, SUM - dq (64-bit) izlaz
 
-section .text
-    mov eax, [N]       ; Učitavanje N u EAX
-    mov ecx, 5         ; Početak petlje od i=5
-    mov edx, 0         ; Inicijalizacija SUM na 0
+SECTION .data
+N   dd 10        ; primer: n = 10 (možete zameniti)
+SUM dq 0
 
-loop_start:
-    cmp ecx, eax       ; Provjeri da li smo stigli do N
-    jg loop_end        ; Ako jeste, izlazimo iz petlje
+SECTION .text
+global _start
 
-    ; Izračunavanje (n + i)/(i - 1)
-    mov ebx, eax       ; Prenos N u EBX
-    add ebx, ecx       ; EBX = N + i
-    sub ecx, 1         ; ECX = i - 1
-    xor edx, edx       ; Postavljanje EDX na 0 za dijeljenje
-    div ecx            ; EAX = (N + i)/(i - 1), EDX = ostatak
-    add edx, eax       ; Dodavanje u SUM
+_start:
+    ; load n (32-bit) -> r9 (signed 64-bit)
+    mov     eax, DWORD [rel N]
+    movsxd  r9, eax        ; r9 = n (signed 64)
 
-    ; Izračunavanje (n - i)^2
-    mov eax, [N]       ; Prenos N u EAX
-    sub eax, ecx       ; EAX = N - i
-    imul eax, eax      ; EAX = (N - i)^2
-    add edx, eax       ; Dodavanje u SUM
+    xor     rsi, rsi       ; rsi = accumulator SUM = 0 (64-bit)
 
-    ; Postavljanje SUM na 64-bitnu lokaciju
-    mov [SUM], rdx
+    ; if n < 5 -> SUM = 0, kraj
+    cmp     r9, 5
+    jl      store_and_exit
 
-    ; Povratak na petlju
-    inc ecx            ; Povećanje i
-    jmp loop_start
+    mov     rcx, 5         ; i = 5 (64-bit)
 
-loop_end:
-    ret
+loop_i:
+    ; --- izračun (n+i)/(i-1)  (64-bit signed div) ---
+    mov     rax, r9        ; rax = n
+    add     rax, rcx       ; rax = n + i
+    cqo                    ; sign-extend rax -> rdx:rax
+    mov     rdi, rcx
+    dec     rdi            ; rdi = i-1
+    idiv    rdi            ; rax = quotient = (n+i)/(i-1)
+    mov     r10, rax       ; r10 = term1
+
+    ; --- izračun (n-i)^2 ---
+    mov     rax, r9        ; rax = n
+    sub     rax, rcx       ; rax = n - i
+    imul    rax, rax       ; rax = (n-i)^2  (64-bit signed multiply safe here)
+    ; sada rax = term2
+
+    add     rax, r10       ; rax = term1 + term2
+
+    add     rsi, rax       ; SUM += term1 + term2
+
+    ; i++
+    inc     rcx
+    cmp     rcx, r9
+    jle     loop_i
+
+store_and_exit:
+    mov     [rel SUM], rsi ; spremi 64-bit SUM u memoriju
+
+    ; exit syscall (return code 0)
+    mov     rax, 60
+    xor     rdi, rdi
+    syscall
