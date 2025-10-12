287d81a5976887dba8c9b2f624fc95af32d002df
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2025-10-12 22:44:45 +0200

    Update KaficSaPlazom.h

--- oop/januar_2024/1_zadatak/KaficSaPlazom.h
+++ oop/januar_2024/1_zadatak/KaficSaPlazom.h
@@ -18,8 +18,8 @@ public:
 
     void Dodaj(ParLezaljki* p);
     void Oslobodi(int rbr);
-    void UkupnoLezaljki() const;
-    void UkupnaSvotaNovca() const;
+    int UkupnoLezaljki() const;
+    double UkupnaSvotaNovca() const;
     bool PostojiGdeTrebaPlatiti(double svota) const;
     void VratiNajskuplje(ParLezaljki** maxCena, ParLezaljki** maxNapici) const;
 
