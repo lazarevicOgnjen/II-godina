a70c198bb2cfe34344f2d4d47059648d92c141f8
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2025-10-12 15:44:07 +0200

    Create ParLezaljkiSaDodatkom.cpp

---
+++ oop/januar_2024/ParLezaljkiSaDodatkom.cpp
@@ -0,0 +1,28 @@
+#include "ParLezaljkiSaDodatkom.h"
+
+ParLezaljkiSaDodatkom::ParLezaljkiSaDodatkom(int r, int rd, double cl, double cn, int bd)
+    : ParLezaljki(r, rd, cl, cn), brojDodatnih(bd) {}
+
+ParLezaljkiSaDodatkom::~ParLezaljkiSaDodatkom() {}
+
+double ParLezaljkiSaDodatkom::UkupnoZaNaplatu() const {
+    double povecanje = 1 + 0.4 * brojDodatnih;
+    double popust = (40 - 2 * red - 5 * brojDodatnih) / 100.0;
+    return cenaLezaljki * povecanje + cenaNapicima * (1 - popust);
+}
+
+void ParLezaljkiSaDodatkom::Prikazi() const {
+    cout << "Par lezaljki + dodatne [rbr=" << rbr
+         << ", red=" << red
+         << ", dodatne=" << brojDodatnih
+         << ", cena lezaljki=" << cenaLezaljki
+         << ", napici=" << cenaNapicima
+         << ", ukupno=" << UkupnoZaNaplatu() << "]" << endl;
+}
+
+ostream& operator<<(ostream& os, const ParLezaljkiSaDodatkom& p) {
+    os << "ParLezaljkiSaDodatkom(" << p.rbr << ", " << p.red << ", "
+       << p.cenaLezaljki << ", " << p.cenaNapicima
+       << ", dodatne=" << p.brojDodatnih << ")";
+    return os;
+}
